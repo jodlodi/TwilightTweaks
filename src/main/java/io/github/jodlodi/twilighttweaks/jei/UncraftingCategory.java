bf56994b8f5c923f921b519e623c2d8eab513e1c
jodi / jodlodi <jodlodi090@gmail.com>
2021-11-17 13:34:32 +0100

    jei fix

--- src/main/java/io/github/jodlodi/twilighttweaks/jei/UncraftingCategory.java
+++ src/main/java/io/github/jodlodi/twilighttweaks/jei/UncraftingCategory.java
@@ -2,8 +2,6 @@ package io.github.jodlodi.twilighttweaks.jei;
 
 import com.google.common.collect.ImmutableList;
 import io.github.jodlodi.twilighttweaks.TwilightTweaks;
-import io.github.jodlodi.twilighttweaks.data.recipes.IUncraftingRecipe;
-import io.github.jodlodi.twilighttweaks.data.recipes.UncraftingRecipe;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.gui.drawable.IDrawable;
@@ -11,6 +9,7 @@ import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.ICraftingRecipe;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
@@ -20,7 +19,8 @@ import twilightforest.block.TFBlocks;
 import java.util.Arrays;
 import java.util.List;
 
-public class UncraftingCategory implements IRecipeCategory<IUncraftingRecipe> {
+public class UncraftingCategory implements IRecipeCategory<ICraftingRecipe> {
+    public static final ResourceLocation id = TwilightTweaks.twilightResource("jei_uncrafting");
     public static final int width = 116;
     public static final int height = 54;
     private final IDrawable background;
@@ -36,12 +36,12 @@ public class UncraftingCategory implements IRecipeCategory<IUncraftingRecipe> {
 
     @Override
     public ResourceLocation getUid() {
-        return UncraftingRecipe.RESOURCE_LOCATION;
+        return id;
     }
 
     @Override
-    public Class<? extends IUncraftingRecipe> getRecipeClass() {
-        return UncraftingRecipe.class;
+    public Class<? extends ICraftingRecipe> getRecipeClass() {
+        return ICraftingRecipe.class;
     }
 
     @Override
@@ -60,65 +60,34 @@ public class UncraftingCategory implements IRecipeCategory<IUncraftingRecipe> {
     }
 
     @Override
-    public void setIngredients(IUncraftingRecipe recipe, IIngredients ingredients) {
+    public void setIngredients(ICraftingRecipe recipe, IIngredients ingredients) {
         ImmutableList.Builder<ItemStack> inputBuilder = ImmutableList.builder();
         ImmutableList.Builder<List<ItemStack>> outputBuilder = ImmutableList.builder();
 
         ItemStack uncraftable = recipe.getResultItem();
         if (!uncraftable.isEmpty()) inputBuilder.add(uncraftable);
 
-        for (Ingredient i : recipe.getIngredients()) outputBuilder.add(Arrays.asList(i.getItems()));
+        for (Ingredient ingredient : recipe.getIngredients()) outputBuilder.add(Arrays.asList(ingredient.getItems()));
 
         ingredients.setInputLists(VanillaTypes.ITEM, ImmutableList.of(inputBuilder.build()));
         ingredients.setOutputLists(VanillaTypes.ITEM, outputBuilder.build());
     }
 
     @Override
-    public void setRecipe(IRecipeLayout recipeLayout, IUncraftingRecipe recipe, IIngredients ingredients) {
-        /*List<List<ItemStack>> inputs = ingredients.getInputs(VanillaTypes.ITEM);
-        List<List<ItemStack>> outputs  = ingredients.getOutputs(VanillaTypes.ITEM);
-        IFocus<?> focus = recipeLayout.getFocus();
-
-        recipeLayout.getItemStacks().init(0, true, 10, 35);
-        recipeLayout.getItemStacks().set(0, getItemMatchingFocus(focus, IFocus.Mode.OUTPUT, outputs.get(0), inputs.get(0)));
-
-        int index = 1, posX = 76 - (inputs.size() * 9);
-        for (int i = 1; i < inputs.size(); i++) {
-            List<ItemStack> o = inputs.get(i);
-            recipeLayout.getItemStacks().init(index, true, posX, 0);
-            recipeLayout.getItemStacks().set(index, o);
-            index++;
-            posX += 18;
-        }
-
-        recipeLayout.getItemStacks().init(7, false, 58, 35);
-        recipeLayout.getItemStacks().set(7, getItemMatchingFocus(focus, IFocus.Mode.INPUT, inputs.get(0), outputs.get(0)));*/
-
-
-
-
-        int index = 0;
-
-
-        for (List<ItemStack> o : ingredients.getOutputs(VanillaTypes.ITEM)) {
-            recipeLayout.getItemStacks().init(index + 1, true, (index % 3) * 18 + 62, (index / 3) * 18);
-            recipeLayout.getItemStacks().set(++index, o);
-        }
-
-        recipeLayout.getItemStacks().init(++index, false, 4, 18);
-        recipeLayout.getItemStacks().set(index, ingredients.getInputs(VanillaTypes.ITEM).get(0));
-    }
-
-
-    /*private List<ItemStack> getItemMatchingFocus(IFocus<?> focus, IFocus.Mode mode, List<ItemStack> focused, List<ItemStack> other) {
-        if (focus != null && focus.getMode() == mode) {
-            ItemStack focusStack = (ItemStack) focus.getValue();
-            for (int i = 0; i < focused.size(); i++) {
-                if (focusStack.equals(focused.get(i))) {
-                    return Collections.singletonList(other.get(i));
-                }
+    public void setRecipe(IRecipeLayout recipeLayout, ICraftingRecipe recipe, IIngredients ingredients) {
+        int i = 0;
+        List<List<ItemStack>> outputs = ingredients.getOutputs(VanillaTypes.ITEM);
+        for (int j = 0, k = 0; j < outputs.size(); j++) {
+            int x = i % 3, y = i / 3;
+            recipeLayout.getItemStacks().init(i + 1, true, x * 18 + 62, y * 18);
+            List<ItemStack> draw = outputs.get(j - k);
+            if (recipe.canCraftInDimensions(y, 3) | recipe.canCraftInDimensions(3, x)) {
+                draw = null;
+                k++;
             }
+            recipeLayout.getItemStacks().set(++i, draw);
         }
-        return other;
-    }*/
+        recipeLayout.getItemStacks().init(++i, false, 4, 18);
+        recipeLayout.getItemStacks().set(i, ingredients.getInputs(VanillaTypes.ITEM).get(0));
+    }
 }
--- src/main/java/io/github/jodlodi/twilighttweaks/jei/UncraftingPlugin.java
+++ src/main/java/io/github/jodlodi/twilighttweaks/jei/UncraftingPlugin.java
@@ -1,19 +1,16 @@
 package io.github.jodlodi.twilighttweaks.jei;
 
 import io.github.jodlodi.twilighttweaks.TwilightTweaks;
-import io.github.jodlodi.twilighttweaks.data.recipes.IShapedUncraftingRecipe;
-import io.github.jodlodi.twilighttweaks.data.recipes.UncraftingRecipe;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaRecipeCategoryUid;
-import mezz.jei.api.registration.IRecipeCatalystRegistration;
-import mezz.jei.api.registration.IRecipeCategoryRegistration;
-import mezz.jei.api.registration.IRecipeRegistration;
-import mezz.jei.api.registration.IRecipeTransferRegistration;
+import mezz.jei.api.registration.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.ICraftingRecipe;
 import net.minecraft.util.ResourceLocation;
 import twilightforest.block.TFBlocks;
+import twilightforest.client.UncraftingGui;
 import twilightforest.inventory.UncraftingContainer;
 
 import java.util.List;
@@ -24,7 +21,7 @@ public class UncraftingPlugin implements IModPlugin {
     @Override
     public ResourceLocation getPluginUid() {
         return TwilightTweaks.twilightResource("uncrafting_jei");
-    }
+    }//TODO:FIX REG "twilighttweaks:uncrafting"
 
     @Override
     public void registerCategories(IRecipeCategoryRegistration registration) {
@@ -33,20 +30,27 @@ public class UncraftingPlugin implements IModPlugin {
 
     @Override
     public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
-        registration.addRecipeCatalyst(new ItemStack(TFBlocks.uncrafting_table.get()), IShapedUncraftingRecipe.RESOURCE_LOCATION);
+        registration.addRecipeCatalyst(new ItemStack(TFBlocks.uncrafting_table.get()), UncraftingCategory.id);
         registration.addRecipeCatalyst(new ItemStack(TFBlocks.uncrafting_table.get()), VanillaRecipeCategoryUid.CRAFTING);
     }
 
     @Override
     public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
-        registration.addRecipeTransferHandler(UncraftingContainer.class, VanillaRecipeCategoryUid.CRAFTING, 2, 9, 12, 36);
+        registration.addRecipeTransferHandler(UncraftingContainer.class, VanillaRecipeCategoryUid.CRAFTING, 11, 9, 20, 36);
     }
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
+        List<ICraftingRecipe> recipes = null;
         if (Minecraft.getInstance().level != null) {
-            List<UncraftingRecipe> recipes = Minecraft.getInstance().level.getRecipeManager().getRecipes().stream().filter(UncraftingRecipe.class::isInstance).map(UncraftingRecipe.class::cast).collect(Collectors.toList());
-            registration.addRecipes(recipes, IShapedUncraftingRecipe.RESOURCE_LOCATION);
+            recipes = Minecraft.getInstance().level.getRecipeManager().getRecipes().stream().filter(ICraftingRecipe.class::isInstance).map(ICraftingRecipe.class::cast).collect(Collectors.toList());
+            registration.addRecipes(recipes, UncraftingCategory.id);
         }
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        registration.addRecipeClickArea(UncraftingGui.class, 34, 33, 27, 20, UncraftingCategory.id);
+        registration.addRecipeClickArea(UncraftingGui.class, 118, 33, 27, 20, VanillaRecipeCategoryUid.CRAFTING);
     }
 }
